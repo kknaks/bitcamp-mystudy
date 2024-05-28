44d7f92c5bc57528e5b700a9e468689266cf13ae
kknaks <benesia93@naver.com>
2024-05-28 16:03:19 +0900

    언어별 Hello World 작성

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello, world!\n");
+
+  int a = 100;
+
+  if (a<100){
+    prontf("hahaha\n");
+  }
+  return 0;
+}
---
+++ hello.js
@@ -0,0 +1,8 @@
+console.log("hello world!!");
+
+let a = 99;
+
+if (a<100){
+    console.log("hahaha");
+}
+console.log("lolol");
