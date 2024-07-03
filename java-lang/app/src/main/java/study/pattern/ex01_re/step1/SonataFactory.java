5034cfc6afb6b73d00423cac90902549e0f2a3fa
kknaks <benesia93@naver.com>
2024-07-04 08:39:01 +0900

    Day28_class_update

--- java-lang/app/src/main/java/study/pattern/ex01_re/step1/SonataFactory.java
+++ java-lang/app/src/main/java/study/pattern/ex01_re/step1/SonataFactory.java
@@ -1,6 +1,17 @@
 package study.pattern.ex01_re.step1;
 
 public class SonataFactory implements CarFactory {
+  private SonataFactory() {}
+
+  private static SonataFactory instance;
+
+  public static SonataFactory getInstance() {
+    if (instance == null) {
+      instance = new SonataFactory();
+    }
+    return instance;
+  }
+
   @Override
   public Sedan createCar() {
     Sedan s = new Sedan();
