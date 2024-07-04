634c25a35c72c7e27d17db9b3623bf5c7c28bd1b
kknaks <benesia93@naver.com>
2024-07-04 18:03:02 +0900

    Day29_class_update

---
+++ java-lang/app/src/main/java/study/thisIsJava/Array.java
@@ -0,0 +1,14 @@
+package study.thisIsJava;
+
+public class Array {
+  public static void main(String[] args) {
+    String[] number = {"1", "2", "3"};
+
+    String[] number2;
+    // number2={"1","2","3"};
+    number2 = new String[] {"1", "2", "3"};
+    String[] number3;
+
+    number3 = number;
+  }
+}
